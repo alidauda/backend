c5c70a945c94e54201de90facd2bc2c7728134e6
alidauda <alidauda14@gmail.com>
2023-08-11 12:36:14 +0100

    remove private keys

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -16,9 +16,3 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 export const db = prisma;
-DATABASE_URL =
-  'mysql://825pusfn1lh1qytjqrjk:pscale_pw_fUQIluWI6LHuqy5u0yDjcBakwEHj6dhthBRn2XuWGAj@aws.connect.psdb.cloud/sis-shop?sslaccept=strict';
-JWT_SECRET_KEY = 'udhcndbdcdjcnkxcbjbcsj';
-REDIS_URL =
-  'redis://default:fbe3d7b6f07f47cc9e21218dafbc167d@novel-midge-46031.upstash.io:46031';
-RESEND_API_KEY = 're_MpSzbXhY_Gapn1FZyyyzBjWp4oCPZ8koD';
